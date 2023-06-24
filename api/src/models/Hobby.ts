c48bd0dee5cd2c88a70657babe68bdba2d9ec569
Miguel <miguel.viloria.247@gmail.com>
2023-06-24 10:11:36 -0400

    hobby

--- api/src/models/Hobby.ts
+++ api/src/models/Hobby.ts
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-import {
-    Table,
-    Column,
-    Model,
-    CreatedAt,
-    UpdatedAt,
-    DeletedAt,
-    PrimaryKey,
-    AutoIncrement,
-    AllowNull,
-    ForeignKey,
-    BelongsTo,
-} from 'sequelize-typescript';
-
-import User from './User';
-
-@Table({
-    tableName: 'hobby',
-    timestamps: true,
-    paranoid: true
-})
-export class Hobby extends Model<Hobby> {
-
-    @PrimaryKey
-    @AutoIncrement
-    @Column
-    id?: number;
-
-    @AllowNull(false)
-    @Column
-    name?: string;
-
-    @CreatedAt
-    @Column
-    createdAt?: Date;
-
-    @UpdatedAt
-    @Column
-    updatedAt?: Date;
-
-    @DeletedAt
-    @Column
-    deletedAt?: Date;
-
-    @ForeignKey(() => User)
-    @Column
-    userId?: number;
-
-    @BelongsTo(() => User)
-    user?: User;
-}
-
-=======
 import {
     Table,
     Column,
@@ -94,5 +40,4 @@ export class Hobby extends Model<Hobby> {
     users?: User[];
 }
 
->>>>>>> origin/master
 export default Hobby;

ef571aa2ee3897ca3b536670a1c28cc8d9bef85e
Miguel <miguel.viloria.247@gmail.com>
2023-06-27 13:36:19 -0400

    recovery a change password, validationReqSchema

--- api/src/routes/userManager/userController.ts
+++ api/src/routes/userManager/userController.ts
@@ -2,7 +2,13 @@ import { where } from 'sequelize';
 import { User } from '../../models/personal_models/User';
 import { Response, Request, NextFunction } from 'express';
 import { comparePassword, hashPassword } from '../../utils/hashPassword';
-import { createJWT } from '../../utils/jwt';
+import { createJWT, verifyJWT } from '../../utils/jwt';
+import { emailForgotPassword } from '../../utils/email';
+
+interface TokenPayload {
+    id: string;
+    name: string;
+}
 
 export default class UserController {
     
@@ -33,9 +39,11 @@ export default class UserController {
             const user: User = req.body;
             if(typeof user.password === 'string'){
                 user.password = hashPassword(user.password);
-            };
-            const newUser = await User.create(user);     
-            return res.status(200).json({ user: newUser, message: 'User registered successfully' });
+                const newUser = await User.create(user);     
+                return res.status(200).json({ user: newUser, message: 'User registered successfully' });
+            } else{
+                return res.status(401).json({ message: 'Invalid password' });
+            }
         } catch (error) {
             return res.status(500).json({ message: 'Error creating user', error });
         }
@@ -65,7 +73,60 @@ export default class UserController {
                 return res.status(401).json({ message: 'Invalid email' });
             }
         } catch (error) {
-            console.error('Authentication error:', error);
+            return res.status(500).json({ message: 'Authentication error:', error });
+        }
+    }
+
+    public recoveryPassword = async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        try {
+            const { email } = req.body;
+            const user: User|null = await User.findOne({ where: {email}});
+            if(user && user?.name){
+                const token = createJWT({
+                    id: user.id,
+                    name: user.name,
+                })
+                await emailForgotPassword({email, name: user.name}, token);
+                return res.status(200).json({ token, message: 'Recovery password email send' });
+            } else{
+                return res.status(401).json({ message: 'Invalid email' });
+            }
+        } catch (error) {
+            return res.status(500).json({ message: 'Recovery password error:', error });
+        }
+    }
+
+    public changePassword =async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        try {
+            const { token } = req.params;
+            const { password } = req.body;
+            const userData = await verifyJWT(token);
+            if (userData && (userData as TokenPayload).id) {
+                const { id, name } = userData as TokenPayload;
+                const user: User | null = await User.findOne({
+                    where: { id },
+                });
+                if(user){
+                    const newPassword = hashPassword(password);
+                    user.password = newPassword
+                    await user.save()
+                    return res.status(200).json({ token, message: 'Password changed succesfully' });
+                } else{
+                    return res.status(401).json({ message: 'User not found' });
+                }
+            } else{
+                return res.status(401).json({ message: 'Invalid token' });
+            }
+        } catch (error) {
+            return res.status(500).json({ message: 'Change password error:', error });
         }
     }
 }
--- api/src/routes/userManager/userRoutes.ts
+++ api/src/routes/userManager/userRoutes.ts
@@ -1,10 +1,37 @@
 import { Router } from 'express';
 import UserController from './userController'
+import validationReqSchema from '../../middlewares/validations';
+import { body, param } from 'express-validator';
+
 const router = Router();
 const userController = new UserController();
 
 router.get('/', userController.allUsers);
-router.post('/register', userController.register);
-router.post('/login', userController.login)
+
+router.post('/register',
+    validationReqSchema([
+        body('name').isString(),
+        body('lastname').isString(),
+        body('email').isEmail(), 
+        body('password').isString(),
+        body('rut').isString(),
+        body('state').isBoolean(),
+        body('online').isBoolean(),
+        body('rol').isString(),
+        body('distributor_name').isString()
+    ]), 
+    userController.register);
+
+router.post('/login', 
+    validationReqSchema([body('email').isEmail(), body('password').isString()]), 
+    userController.login);
+
+router.post('/recovery',
+    validationReqSchema([body('email').isEmail()]), 
+    userController.recoveryPassword);
+
+router.put('/changePassword/:token',
+    validationReqSchema([body('password').isString(), param('token').isJWT()]), 
+    userController.changePassword);
 
 export default router;
